a1bb4826a9f0553c6e72e2586797eca60a6e4762
Asir Selvasingh <me@selvasingh.com>
2023-07-02 15:49:36 -0700

    draft to deploy Spring Native apps

---
+++ azure/setup-env-variables-template.sh
@@ -0,0 +1,10 @@
+export SUBSCRIPTION=subscription-id                 # replace it with your subscription-id
+export RESOURCE_GROUP=resource-group-name           # existing resource group or one that will be created in next steps
+export SPRING_APPS_SERVICE=azure-spring-apps-name   # name of the service that will be created in the next steps
+export LOG_ANALYTICS_WORKSPACE=log-analytics-name   # existing workspace or one that will be created in next steps
+export REGION=region-name                           # choose a region with Enterprise tier support
+
+export PETCLINIC_JAR_APP="petclinic-jar"
+export PETCLINIC_NATIVE_APP="petclinic-native"
+
+export CUSTOM_BUILDER="no-bindings-builder"
